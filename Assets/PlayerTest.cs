e9e44219ac033708b58a83862ac9f35c897ce45e
gyul-is-yummy <gyulisyummy@gmail.com>
2025-02-24 20:02:31 +0900

    Feat: 좀비 애니메이션 구현
    Scene_Temp_YHJ 씬에서 임시 플레이어로 테스트 가능

--- Assets/PlayerTest.cs
+++ Assets/PlayerTest.cs
@@ -7,9 +7,6 @@ public class PlayerTest : ObjectController
         var horizontal = Input.GetAxisRaw("Horizontal");
         var vertical = Input.GetAxisRaw("Vertical");
         direction = new Vector2(horizontal, vertical).normalized;
-        Debug.Log(direction == Vector2.zero);
-
-        if (direction == Vector2.zero) return;
 
         if (Input.GetKeyDown(KeyCode.Space) && transform.position.z == 0)
         {
--- Assets/Scripts/Controller/EnemyController.cs
+++ Assets/Scripts/Controller/EnemyController.cs
@@ -5,19 +5,49 @@ using static ObjectController;
 
 public class EnemyController : ObjectController
 {
-    private void Update()
+    protected override void Initialize()
     {
-        //direction에 값 넣어주면 이동
+        base.Initialize();
 
-        //만약 플레이어와의 거리가 일정 이상 가까워진다면
-        //Attack()으로 넘어감
+        //테스트를 위한 임시 코드
+        PlayerTest player = GetComponent<PlayerTest>();
+
     }
+
 
     public override void Attack()
     {
         base.Attack();
 
+        Debug.Log("Zombie Attack");
 
+        //투사체를 날리는 애들이라면 투사체 생성 로직 (날아가는건 별개 스크립트로)
+            //예시: n초마다 한 번씩 투사체를 날린다.
+
+        //좀비는 근접 공격이니까 충돌한 순간 플레이어의 HP를 깎아주면 될 것임
+    }
+
+    public void OnDamage()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //충돌한 오브젝트가 플레이어라면 (자주 쓸 것 같아서 define에 추가해야 할 듯)
+        if (collision.collider.CompareTag("Player"))
+        {
+            //공격 (근접 몹 한정)
+            Attack();
+        }
+    }
+
+    private void Update()
+    {
+        //direction에 값 넣어주면 이동
+
+        //플레이어를 향해 이동하려면 or 플레이어를 향해 발사체를 날리려면
+        //플레이어의 위치를 알 수 있어야 함
     }
 
 
