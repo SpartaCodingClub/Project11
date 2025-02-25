9e3d338f0ed72de4d246a838d730d5745a217ace
SpartaCodingClub <spartacodingclub.kr@gmail.com>
2025-02-25 10:38:04 +0900

    Chore: 폴더 구조 정리

--- Assets/Scripts/Managers/SceneManager/Scene_Lobby.cs
+++ Assets/Scripts/Managers/SceneManager/Scene_Lobby.cs
@@ -1,7 +1,12 @@
+using UnityEngine;
+
 public class Scene_Lobby : Scene_Base
+
 {
     protected override void Initialize()
     {
         base.Initialize();
+
+        Managers.Resource.Instantiate<PlayerController>(null, Vector2.zero);
     }
 }
