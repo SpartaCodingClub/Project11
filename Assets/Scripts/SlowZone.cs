cb0ded50160c2a099ca1350f1ad32a26ee9ee2ea
KwonSumin <rbalstnals1920@gmail.com>
2025-02-26 10:25:01 +0900

    Feat 느려지는 구간 생성

---
+++ Assets/Scripts/SlowZone.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class SlowZone : MonoBehaviour
+{
+    public float slowMultiplier = 0.4f; // 속도를 줄이는 비율
+    private float originalSpeed;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        StatHandler statHandler = other.GetComponent<StatHandler>();
+        if (statHandler != null)
+        {
+            originalSpeed = statHandler.MoveSpeed;
+            statHandler.MoveSpeed *= slowMultiplier; // 속도 감소
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        StatHandler statHandler = other.GetComponent<StatHandler>();
+        if (statHandler != null)
+        {
+            statHandler.MoveSpeed = originalSpeed; // 원래 속도로 복구
+        }
+    }
+}
