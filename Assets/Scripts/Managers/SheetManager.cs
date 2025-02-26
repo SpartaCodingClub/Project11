f48764030c365b593a932b3c544c7e6e9f30cfd8
SpartaCodingClub <spartacodingclub.kr@gmail.com>
2025-02-26 10:54:47 +0900

    Fix: 시트 매니저 임시 비활성화
    시트 작업 중이라 오류나여 ㅠ

--- Assets/Scripts/Managers/SheetManager.cs
+++ Assets/Scripts/Managers/SheetManager.cs
@@ -1,15 +1,23 @@
+using GoogleSheet.Core.Type;
 using UGS;
 
 public class SheetManager
 {
+    [UGS(typeof(SkillType))]
+    public enum SkillType
+    {
+        StatHandler,
+        ProjectileHandler
+    }
+
     public void Initialize()
     {
-        UnityGoogleSheet.LoadFromGoogle<int, SkillTable.Data>((list, map) =>
-        {
-            list.ForEach(x =>
-            {
-                Debug.Log($"{x.index} : {x.intValue}, {x.strValue}");
-            });
-        }, true);
+        //UnityGoogleSheet.LoadFromGoogle<int, SkillTable.Data>((list, map) =>
+        //{
+        //    list.ForEach(x =>
+        //    {
+        //        Debug.Log($"{x.index} : {x.Damage}, {x.attackRange}, {x.projectileCount}");
+        //    });
+        //}, true);
     }
 }
