8533170b76c861092591f67dc8c44bd037270eb9
gyul-is-yummy <gyulisyummy@gmail.com>
2025-02-26 13:06:28 +0900

    Feat: Cone 기능 구현
    - Cone을 배치하면 하얀색, 검은색 중에서 랜덤으로 나타남. (매번 다름)
    - Bullet 태그가 붙은 Object로 Cone과 충돌하면 Cone이 쓰러짐(Death)
    - 총알 방향에 따라 쓰러지는 sprite가 달라짐. (현재 위/아래 2방향)
    - Cone이 죽으면 Collider를 꺼서 플레이어와 충돌되지 않게끔 함
    
    추가 예정:
    - 총알 방향에 따라 뒤로 밀리는 기능
    - (여유가 된다면) 콘이 쓰러지는 sprite를 8방향 리터칭 후 추가
    - 프리팹화

--- Assets/Scripts/Controller/ConeController.cs
+++ Assets/Scripts/Controller/ConeController.cs
@@ -4,20 +4,25 @@ using UnityEngine;
 
 public class ConeController : ObjectController
 {
+    Animator animator;
+
     public float random;
     public bool isMove;
+
+    Vector2 direction;
 
     protected override void Initialize()
     {
         base.Initialize();
 
-        //흰색 콘인지 검은색 콘인지 결정
+        //랜덤으로 흰색 콘인지 검은색 콘인지 결정
         random = Random.Range(0f, 1f);
         Debug.Log($"현재 random 값: {random}");
-        random = Mathf.Round(random);
-        Debug.Log($"반올림 후의 random 값: {random}");
 
-        //animationHandler.Animator.Play(Define.Stand, 0, random);
+        //animationHandler를 사용하면 Nullreference가 떠서 새로 받아옴
+        animator = GetComponentInChildren<Animator>();
+        animator.SetFloat("MotionTime", random);
+
     }
 
 
@@ -25,9 +30,21 @@ public class ConeController : ObjectController
     {
         if(collision.collider.CompareTag("Bullet"))
         {
+            collision.rigidbody.mass = 10;
+
+
+            direction = (collision.transform.position - transform.position).normalized;
+
+            animator.SetFloat("X", direction.x);
+            animator.SetFloat("Y", direction.y);
+
+            Debug.Log($"콘을 때린 방향: X:{direction.x} Y:{direction.y}");
             Debug.Log("콘 쓰러짐");
+
+            //쓰러지면 충돌처리가 되지 않게끔
+            this.GetComponent<Collider2D>().enabled = false;
+
             Death();
-            animationHandler.Animator.SetFloat("ConeType", random);
         }
     }
 
