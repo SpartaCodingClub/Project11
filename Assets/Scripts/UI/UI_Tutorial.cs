7f8f6404fb6ce02874571ded6c984d3a3ab17a50
SpartaCodingClub <spartacodingclub.kr@gmail.com>
2025-02-28 00:57:29 +0900

    Fix: 스크립트 오류 수정

--- Assets/Scripts/UI/UI_Tutorial.cs
+++ Assets/Scripts/UI/UI_Tutorial.cs
@@ -131,7 +131,7 @@ public class UI_Tutorial : UI_SubItem
             }
             else
             {
-                Managers.Audio.Play(Clip.SoundFX_TypingSound02);
+                Managers.Audio.Play(Clip.SoundFX_TypingSound);
                 yield return shortInterval;
             }
         }
