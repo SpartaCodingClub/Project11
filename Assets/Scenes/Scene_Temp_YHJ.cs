acef0696741b64c56051740994a5613eb252b692
gyul-is-yummy <gyulisyummy@gmail.com>
2025-02-27 10:47:48 +0900

    WIP: 커밋 받기 전 푸시 (2)

--- Assets/Scenes/Scene_Temp_YHJ.cs
+++ Assets/Scenes/Scene_Temp_YHJ.cs
@@ -7,7 +7,7 @@ public class Scene_Temp_YHJ : Scene_Base
         base.Initialize();
 
         Managers.Resource.Instantiate<PlayerController>(null, Vector2.zero);
-        Managers.Resource.Instantiate("Mushroom", null, 5.0f * Vector2.one, Define.ENEMIES);
+        Managers.Resource.Instantiate("Spider", null, 5.0f * Vector2.one, Define.ENEMIES);
 
     }
 }
